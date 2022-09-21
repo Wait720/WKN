6c7f48632eb39426bf0b901cb6e6086faaac17e7
Wait720 <113753013+Wait720@users.noreply.github.com>
2022-09-21 17:27:40 +0300

    Add files via upload

---
+++ riv.py
@@ -0,0 +1,7 @@
+import math
+from tkinter import Y
+x=float(input("Введіть х:"))
+y=float(input("Введіть у:"))
+def w(x,y):
+    print(math.sin(x)-math.cos(y)*4.138*math.log(abs(math.sin(math.pi/4+2.31*x))))
+w(x,y)
