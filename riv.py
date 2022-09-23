bb14530c01b27b9d4cd1c468ebd272b80d2b42ee
Wait720 <113753013+Wait720@users.noreply.github.com>
2022-09-23 13:16:20 +0300

    Update riv.py

--- riv.py
+++ riv.py
@@ -1,6 +1,6 @@
 import math
-x=float(input("Введіть х:"))
-y=float(input("Введіть у:"))
+x1=float(input("Введіть х:"))
+y1=float(input("Введіть у:"))
 def w(x,y):
     print(math.sin(x)-math.cos(y)*4.138*math.log(abs(math.sin(math.pi/4+2.31*x))))
-w(x,y)
+w(x1,y1)
